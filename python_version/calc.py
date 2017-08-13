2101e0acabdda9fedd40d88adaa624f0fc29a343
Benjamin Chang <ben.chang@hotmail.co.uk>
2017-08-13 17:23:37 +0100

    can evalutate a two float one operator input, working on increasing functionality to any expression regardless of length

--- python_version/calc.py
+++ python_version/calc.py
@@ -21,22 +21,62 @@ def evaluate(user_input):
     '''
     Evaluate user input
     '''
-    for i in check_float(user_input):
-        if i == check_float(user_input)[0]:
-            output_text = i
-        elif i == check_float(user_input)[1]:
-            user_input = i
+    output_text = ''
+    split_input = user_input.split()
+    for i in split_input:
+        if len(check_valid_input(i)) == 2:
+            split_input[split_input.index(i)] = check_valid_input(i)[1]
+        else:
+            split_input[split_input.index(i)] = check_valid_input(i)[0]
+
+    if len(split_input) >= 3:
+        x_int = split_input[0]
+        y_int = split_input[2]
+
+        if split_input[1] == '+':
+            evaluated = x_int + y_int
+        elif split_input[1] == '-':
+            evaluated = x_int - y_int
+        elif split_input[1] == '*':
+            evaluated = x_int * y_int
+        elif split_input[1] == '/':
+            evaluated = x_int / y_int
+        else:
+            output_text += 'Invalid operator'
+        output_text += str(evaluated)
+    else:
+        output_text += 'Invalid expression: {0}'.format(str(user_input))
+
     print "out > " + output_text
 
 # UTILITIES
-def check_float(user_input):
+def check_if_operator(input_data):
+    '''
+    Checks if input is operator
+    '''
+    if input_data == '+':
+        return '+'
+    elif input_data == '-':
+        return '-'
+    elif input_data == '*':
+        return '*'
+    elif input_data == '/':
+        return '/'
+    else:
+        return False
+
+def check_valid_input(input_data):
     '''
     Checks if input is float
     '''
     try:
-        return [user_input, float(user_input)]
+        return [input_data, float(input_data)]
     except ValueError:
-        return ['Please enter a valid input (0-9, +, -, *, /)']
+        output = check_if_operator(input_data)
+        if output is False:
+            return ['Please enter a valid input (0-9, +, -, *, /)']
+        else:
+            return output
 
 
 
